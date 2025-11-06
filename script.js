d764340c5dd5f6f7e7d7e3cfda2e424c498c209f
carles insomne <insomne.nihilista@gmail.com>
2025-11-05 18:31:08 -0600

    Se agregó el botón SUBIR con JS y también se agregaron las 4 imagenes del blog

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// BOTÓN VOLVER ARRIBA - Las Timar
+  const btn = document.getElementById('btn-volver-arriba');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 500) {
+      btn.classList.add('visible');
+    } else {
+      btn.classList.remove('visible');
+    }
+  });
+
+  btn.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
